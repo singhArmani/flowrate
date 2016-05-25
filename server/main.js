7187a08119f4641117e4f023af7909e9df22bf03
Aman Singh <singh_aman@hotmail.com.au>
2016-05-25 16:40:35 +1000

    Date of Insert in FlowSamplesMinutely, changed

--- server/main.js
+++ server/main.js
@@ -34,18 +34,22 @@ var startLoggingFlowPerMinute = () => {
 
        //loop through the FlowSamples Collection
         var d = new Date();
-        console.log(d)
-        var FlowArray = FlowSamples.find({DateofInsert:{$lte:new Date(d.getTime()- 2*60*1000)}}).fetch(); //2 min earlier
+        console.log(d);
+
+        //Check if there's any document in FlowSamples collection which is one month old
+        var FlowArray = FlowSamples.find({DateofInsert:{$lte:new Date(d.getTime()- 2*60*1000)}}).fetch(); //1 month ago
         console.log(FlowArray);
 
-        //or we can loop through FlowSamples and look for entries less than an hour
 
         var TotalFlow = 0;
-        if(FlowArray.length>0) {
 
+        //if there's any document one month older than the date d
+        if(FlowArray.length>0) {
+            var maxDate = 0;
             for(i=1;i<FlowArray.length; i++){
                var a =  FlowArray[i].DateofInsert.getTime() - FlowArray[i-1].DateofInsert.getTime();
                 TotalFlow += FlowArray[i].FlowRate * a / 1000;
+                if(FlowArray[i].DateofInsert.getTime() > maxDate) maxDate = FlowArray[i].DateofInsert.getTime();
             }
 
             console.log("total flow in last minute is "+TotalFlow);
@@ -53,14 +57,14 @@ var startLoggingFlowPerMinute = () => {
             //Inserting
             FlowSamplesMinutely.insert({
                 Flow: TotalFlow+'L',
-                DateofInsert:new Date()
+                DateofInsert:new Date(d.getTime()- 2*60*1000)
             });
 
             //Removing the old documents from the FlowSamples collection which are already saved
             FlowSamples.remove({DateofInsert:{$lte:new Date(d.getTime()- 2*60*1000)}});
         }
 
-    },10000);
+    },60000);
 }
 
 
