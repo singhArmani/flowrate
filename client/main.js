967e0d2d810b66413c61d933960ea165e65841ac
Aman Singh <singh_aman@hotmail.com.au>
2016-05-31 15:28:31 +1000

    Added the minDate, maxDate, and timeInterval Flow

--- client/main.js
+++ client/main.js
@@ -4,3 +4,9 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 
+Template.FlowRate.helpers({
+    getFlowRate: () =>{
+        var d = new Date(); //current millisecond since 1970 jan
+        return  FlowSamples.find({DateofInsert:{$gte:new Date(d.getTime()- 500)}}).fetch();
+    }
+});
--- server/main.js
+++ server/main.js
@@ -7,6 +7,7 @@ Meteor.startup(() => {
   startLoggingFlowRatePerHalfSecond(flowRate,FlowSamples);  //per half second
 
     startLoggingFlowPerMinute();
+    //startLoggingFlowPerHour();
 });
 
 
@@ -34,10 +35,9 @@ var startLoggingFlowPerMinute = () => {
 
        //loop through the FlowSamples Collection
         var d = new Date();
-        console.log(d);
-
-        //Check if there's any document in FlowSamples collection which is one month old
-        var FlowArray = FlowSamples.find({DateofInsert:{$lte:new Date(d.getTime()- 2*60*1000)}}).fetch(); //1 month ago
+        
+        //Check if there's any document in FlowSamples collection which is 2 minute old and runs in interval of 30 seconds
+        var FlowArray = FlowSamples.find({DateofInsert:{$lte:new Date(d.getTime()- 1*60*1000)}}).fetch(); //1 minute ago
         console.log(FlowArray);
 
 
@@ -45,28 +45,72 @@ var startLoggingFlowPerMinute = () => {
 
         //if there's any document one month older than the date d
         if(FlowArray.length>0) {
-            var maxDate = 0;
+            
+            var minDate = 0, maxDate = 0;
+            
             for(i=1;i<FlowArray.length; i++){
-               var a =  FlowArray[i].DateofInsert.getTime() - FlowArray[i-1].DateofInsert.getTime();
+                
+                minDate = FlowArray[0].DateofInsert.getTime(); //let assume the first element is the minimum date
+                
+                var a =  FlowArray[i].DateofInsert.getTime() - FlowArray[i-1].DateofInsert.getTime();
                 TotalFlow += FlowArray[i].FlowRate * a / 1000;
-                if(FlowArray[i].DateofInsert.getTime() > maxDate) maxDate = FlowArray[i].DateofInsert.getTime();
+                
+                if(FlowArray[i].DateofInsert.getTime() < minDate) minDate = FlowArray[i].DateofInsert.getTime(); //getting the minDate of Insert
+                
+                if(FlowArray[i].DateofInsert.getTime() > maxDate) maxDate = FlowArray[i].DateofInsert.getTime(); //getting the maxDate of Insert
+            }
+            
+            //Debugging Purpose
+            console.log("min date is "+new Date(minDate));
+            console.log("max date is "+new Date(maxDate));
+            console.log("the flow was in timeinterval of "+(maxDate-minDate)/1000 +"seconds");
+
+            //Inserting
+            FlowSamplesMinutely.insert({
+                Flow: TotalFlow+'L',
+                DateofInsert:new Date(d.getTime()- 1*60*1000)
+            });
+
+            //Removing the old documents from the FlowSamples collection which are already saved
+            FlowSamples.remove({DateofInsert:{$lte:new Date(d.getTime()- 1*60*1000)}});
+        }
+
+    },20000);
+}
+
+
+var startLoggingFlowPerHour = () =>{
+    Meteor.setInterval(() =>{
+        var d = new Date();
+
+        //Check if there's any document in FlowSamplesMinutely collection which is one month old
+        var FlowArray = FlowSamplesMinutely.find({DateofInsert:{$lte:new Date(d.getTime()- 4*60*1000)}}).fetch();  //4 minute ago for testing purpose
+
+        var TotalFlow = 0;
+
+        //if there's any document one month older than the date d
+        if(FlowArray.length>0) {
+            var minDate = 0;
+            for(i=1;i<FlowArray.length; i++){
+                var a =  FlowArray[i].DateofInsert.getTime() - FlowArray[i-1].DateofInsert.getTime();
+                TotalFlow += FlowArray[i].FlowRate * a / 1000;
+                if(FlowArray[i].DateofInsert.getTime() > minDate) minDate = FlowArray[i].DateofInsert.getTime();
             }
 
             console.log("total flow in last minute is "+TotalFlow);
 
             //Inserting
-            FlowSamplesMinutely.insert({
+            FlowSamplesHourly.insert({
                 Flow: TotalFlow+'L',
-                DateofInsert:new Date(d.getTime()- 2*60*1000)
+                DateofInsert:new Date(d.getTime()- 4*60*1000)
             });
 
-            //Removing the old documents from the FlowSamples collection which are already saved
-            FlowSamples.remove({DateofInsert:{$lte:new Date(d.getTime()- 2*60*1000)}});
+            //Removing the old documents from the FlowSamplesMinutely collection 
+            FlowSamplesMinutely.remove({DateofInsert:{$lte:new Date(d.getTime()- 4*60*1000)}});
         }
 
     },60000);
 }
-
 
 
 
